458d4ac677e2c726d6d00d6cc2003b4bdf028a66
Jenna <166531909+jenna-studio@users.noreply.github.com>
2026-03-11 09:16:42 +0000

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int A, B;
+    int T;
+
+    cin >> T;
+
+    for (int i=1; i<=T; i++) {
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+
+    return 0;
+}
