91e02bcf7f8f3091602bfb12bce69cbe66805953
Jenna <166531909+jenna-studio@users.noreply.github.com>
2026-03-21 17:43:05 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   while (true) {
+       int a, b, c;
+       cin >> a >> b >> c;
+
+       if ( a == 0 && b == 0 && c == 0 ) {
+           break;
+       }
+
+       int max;
+       if (a >= b && a >= c) {
+           max = a;
+       } else if (b >= a && b >= c) {
+           max = b;
+       } else {
+           max = c;
+       }
+
+       if (max == a) {
+           if (a*a == b*b + c*c) {
+               cout << "right" << '\n';
+           } else {
+               cout << "wrong" << '\n';
+           }
+       } else if (max == b) {
+           if (b*b == a*a + c*c) {
+               cout << "right" << '\n';
+           } else {
+               cout << "wrong" << '\n';
+           }
+       } else {
+           if (c*c == a*a + b*b) {
+               cout << "right" << '\n';
+           } else {
+               cout << "wrong" << '\n';
+           }
+       }
+
+   }
+
+    return 0;
+}
